5ac7033937e4bed3a56c322e08373e8175b4ddd1
Klavikus <912654q@gmail.com>
2024-07-19 16:54:57 +0300

    Add follow camera

--- Assets/Scripts/GameCore/Gameplay/Services/GameplayCamera.cs
+++ Assets/Scripts/GameCore/Gameplay/Services/GameplayCamera.cs
@@ -1,11 +1,16 @@
-﻿using UnityEngine;
+﻿using Cinemachine;
+using UnityEngine;
 
 namespace GameCore.Gameplay.Services
 {
     public class GameplayCamera : MonoBehaviour
     {
+        [SerializeField] private CinemachineVirtualCamera _camera;
+
         public void FocusTo(Transform targetTransform)
         {
+            _camera.Follow = targetTransform;
+            _camera.LookAt = targetTransform;
         }
     }
 }
